214429d4b01ec4959e355c4f9780377764ebce3b
Jose Maria <josemarimaya@gmail.com>
2023-06-24 18:59:47 +0200

    minor changes in dates

--- 14_dates.py
+++ 14_dates.py
@@ -10,3 +10,21 @@ print(now)
 # Llamadas a distintas funciones de date
 print(now.hour)
 print(now.year)
+
+timestamp = now.timestamp()
+
+print(timestamp)
+
+summer = datetime(2023, 6, 23)
+
+
+def print_date(fecha):
+    print(fecha.year)
+    print(fecha.month)
+    print(fecha.day)
+    print(fecha.hour)
+    print(fecha.minute)
+    print(fecha.second)
+
+
+print_date(now)
