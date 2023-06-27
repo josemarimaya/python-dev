82ca67c7d045efd8514e927a31b920717ca62f7e
Jose Maria <josemarimaya@gmail.com>
2023-06-27 20:40:26 +0200

    End with lambdas and begin with superior functions

--- 17_lambdas.py
+++ 17_lambdas.py
@@ -1,2 +1,16 @@
 ## LAMBDAS ##
 
+# En resumidas cuentas es una función que se puede almacenar en una variable
+
+sum_values = lambda first, second: first + second
+
+print(sum_values(6, 4))
+
+
+# Se pueden usar lambdas dentro de otras funciones
+
+def multi(number1, number2):
+    return sum_values(number1, number2) * number1
+
+
+print(multi(5, 5))
